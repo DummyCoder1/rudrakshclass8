315492110525112ab9c3802ba7582adca84019e8
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-06 12:51:35 +0530

    Add files via upload

---
+++ scratch_canvas (2).py
@@ -0,0 +1,10 @@
+import turtle
+
+n = int(input(''))
+l = int(input(''))
+def polygon():
+    for variable in range (0,n):
+        turtle. forwerd(l)
+        turtle.left(360 / n)
+                        
+polygon()                        
